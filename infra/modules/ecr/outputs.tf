2cffb80c5da99eb2a2e39f9a549431eb467e6e23
Dave Perera <dave.s.perera@gmail.com>
2026-06-16 04:34:39 +0530

    Add variable and output definitions for ECR, ECS, IAM, and S3 modules

--- infra/modules/ecr/outputs.tf
+++ infra/modules/ecr/outputs.tf
@@ -1,7 +1,3 @@
-variable "repo_name" {
-  type = string
-}
-
 output "repository_url" {
   value = aws_ecr_repository.this.repository_url
 }
---
+++ infra/modules/ecs/variables.tf
@@ -0,0 +1,6 @@
+variable "project"            { type = string }
+variable "aws_region"         { type = string }
+variable "ecr_image_uri"      { type = string }
+variable "task_role_arn"      { type = string }
+variable "execution_role_arn" { type = string }
+variable "s3_bucket_name"     { type = string }
